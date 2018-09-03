ae573ab924b1894db3f467c56ad47f71528bad1d
sm-svyat <sm.svyat@yandex.ru>
2018-09-03 16:21:16 +0300

    Added README and setup files

---
+++ README.md
@@ -0,0 +1,8 @@
+# Intent Classifier
+
+## Authors
+
+* Dmitry Ischenko
+* Yana Balandyuk-Opalinskaya
+* Vladimir Sveshnikov
+* Svyatoslav Nevyantsev
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='intentclf',
+    version='0.0.1',
+    description='Intent Classifier',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='The MIT License',
+    url='https://github.com/RobotVeraDS/intent-classifier',
+    author='Robot Vera',
+    author_email='sm.svyat@yandex.ru',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+    ],
+
+    packages=['intentclf'],
+
+    keywords='nlp',
+    install_requires=[],
+)
