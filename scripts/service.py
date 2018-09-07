be86691202fc911814f73b5829056ffef0cdc941
Dmitry Ischenko <ischenko.dmitry@gmail.com>
2018-09-07 10:54:11 +0000

    Updated test script. Remove not used jupyter notebooks

--- scripts/service.py
+++ scripts/service.py
@@ -4,15 +4,20 @@ from intentclf.models import IntentClassifier
 import json
 
 embedder = Embedder("/mnt/disk/models/glove-hh-embeds.kv")
-
-text = "привет как дела? У меня хорошо"
-embedder.get_vector("привет как дела")
-
-
 classifier = IntentClassifier(embedder)
 
 with open("data/dialog.ru.32.json", "r") as handle:
     data = json.load(handle)
 
 classifier.train(data)
-print(classifier.predict("есть ли медицинская страховка?"))
+
+
+questions = [
+    "какая у вас зарплата?",
+    "можно ли обучаться в компании?",
+    "работать по выходным можно?"
+]
+
+for question in questions:
+    answer = classifier.predict(question)
+    print("{} : {}".format(question, answer))
