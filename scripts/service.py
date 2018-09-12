f841b4e4bcd9491ef6040b5c50dff5ce413f52fe
sm-svyat <sm.svyat@yandex.ru>
2018-09-12 20:00:25 +0300

    Fixed line indents and function names

--- scripts/service.py
+++ scripts/service.py
@@ -9,9 +9,11 @@ from intentclf.models import IntentClassifier
 models_storage = 'scripts/models/'
 
 print("Open log file...")
-logging.basicConfig(filename='scripts/logs/log.txt',
-                    format='%(asctime)s %(message)s',
-                    level=logging.DEBUG)
+logging.basicConfig(
+    filename='scripts/logs/log.txt',
+    format='%(asctime)s %(message)s',
+    level=logging.DEBUG
+)
 
 print("Load embedder...")
 embedder = Embedder(os.environ['INTENT_CLASSIFIER_MODEL'])
@@ -20,53 +22,66 @@ print("Prepare app...")
 app = Flask(__name__)
 
 
-def get_models_ids(path):
-    models = [f_name for f_name in os.listdir(models_storage) if
-              ('pickle' in f_name)]
-    models_ids = map(lambda m: int(m.split('model-')[1].split('.pickle')[0]),
-                     models)
+def get_sorted_models_ids(path):
+    models = [
+        file_name for file_name in os.listdir(models_storage) if (
+            'pickle' in file_name
+        )
+    ]
+
+    models_ids = map(
+        lambda m: int(m.split('model-')[1].split('.pickle')[0]),
+        models
+        )
     return sorted(models_ids)
 
 
 @app.route("/answer", methods=["GET"])
 def answer():
-    if (request.method != "GET" or
-            "id" not in request.args or
-            "question" not in request.args):
+    if (
+        request.method != "GET" or
+        "id" not in request.args or
+        "question" not in request.args
+    ):
         return jsonify({
-            "message": "Please sent GET query with `id` and `question` keys"
+            "message": "Please sent GET query with `id` and `question` keys",
         })
 
     classifier = IntentClassifier(embedder)
 
     model_id = request.args.get("id")
 
-    if int(model_id) in get_models_ids(models_storage):
+    if int(model_id) in get_sorted_models_ids(models_storage):
         classifier.load(
             "scripts/models/model-{}.pickle".format(model_id)
         )
     else:
         return jsonify({
-            "message": "Model with ID {} not found".format(model_id)
+            "message": "Model with id {} not found".format(model_id),
         })
 
-    logging.info('predicting intent for question: {}'.format(
-                request.args.get("question")))
+    logging.info(
+        'predicting intent for question: {}'.format(
+            request.args.get("question")
+        )
+    )
     answer = classifier.predict(request.args.get("question"))
 
     logging.info('predicted intent: {}'.format(answer))
 
     return jsonify({
-        "answer": answer
+        "answer": answer,
     })
 
 
 @app.route("/train", methods=["GET", "POST"])
 def train():
-    if (request.method not in ["GET", "POST"] or
-            "intentjson" not in request.args):
+    if (
+        request.method not in ["GET", "POST"] or
+        "intentjson" not in request.args
+    ):
         return jsonify({
-            "message": "Please sent GET or POST query with `intentjson` keys"
+            "message": "Please sent GET or POST query with `intentjson` keys",
         })
     intentclf = IntentClassifier(embedder)
 
@@ -77,7 +92,7 @@ def train():
         data = json.loads(request.args["intentjson"])
     except:
         return jsonify({
-            "message": "Please send correct json object"
+            "message": "Please send correct json object",
         })
 
     for label, category in enumerate(data):
@@ -89,15 +104,16 @@ def train():
 
     if len(data) < 2:
         return jsonify({
-            "message": "For training the model requires 2 or more intents"
+            "message": "For training the model requires 2 or more intents",
         })
 
-    new_model_id = get_models_ids(models_storage)[-1] + 1
+    new_model_id = get_sorted_models_ids(models_storage)[-1] + 1
     intentclf.save(models_storage + 'model-{}.pickle'.format(new_model_id))
-    logging.info('saved model with ID {}'.format(new_model_id))
+    logging.info('saved model with id {}'.format(new_model_id))
 
     return jsonify({
-        "message": 'Created model with ID {}'.format(new_model_id)
+        "message": 'Created model with id {}'.format(new_model_id),
+        "id": new_model_id,
     })
 
 
