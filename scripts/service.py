eba715e1aba9dad017f3c81e1308e892eebf3139
sm-svyat <sm.svyat@yandex.ru>
2018-09-12 17:35:49 +0300

    Modified web service
    1. Added web interface for creating intent models.
    2. Сhanged process of logging.

--- scripts/service.py
+++ scripts/service.py
@@ -1,15 +1,32 @@
 from flask import Flask, request, jsonify
 import os
+import json
 import datetime
+import logging
 
 from intentclf.models import Embedder
 from intentclf.models import IntentClassifier
+
+models_storage = 'scripts/models/'
+
+print("Open log file...")
+logging.basicConfig(filename='scripts/logs/log.txt',
+                    format='%(asctime)s %(message)s',
+                    level=logging.DEBUG)
 
 print("Load embedder...")
 embedder = Embedder(os.environ['INTENT_CLASSIFIER_MODEL'])
 
 print("Prepare app...")
 app = Flask(__name__)
+
+
+def get_models_ids(path):
+    models = [f_name for f_name in os.listdir(models_storage) if
+              ('pickle' in f_name)]
+    models_ids = map(lambda m: int(m.split('model-')[1].split('.pickle')[0]),
+                     models)
+    return sorted(models_ids)
 
 
 @app.route("/answer", methods=["GET"])
@@ -22,21 +39,66 @@ def answer():
         })
 
     classifier = IntentClassifier(embedder)
-    classifier.load(
-        "scripts/models/model-{}.pickle".format(request.args.get("id"))
-    )
 
+    model_id = request.args.get("id")
+
+    if int(model_id) in get_models_ids(models_storage):
+        classifier.load(
+            "scripts/models/model-{}.pickle".format(model_id)
+        )
+    else:
+        return jsonify({
+            "message": "Model with ID {} not found".format(model_id)
+        })
+
+    logging.info('predicting intent for question: {}'.format(
+                request.args.get("question")))
     answer = classifier.predict(request.args.get("question"))
 
-    with open("scripts/logs/log.txt", "a") as log_file:
-        print("{}: {}\t{}".format(
-            datetime.datetime.now(),
-            request.args.get("question"),
-            answer
-        ), file=log_file)
+    logging.info('predicted intent: {}'.format(answer))
 
     return jsonify({
         "answer": answer
+    })
+
+
+@app.route("/train", methods=["GET", "POST"])
+def train():
+    if (request.method not in ["GET", "POST"] or
+            "intentjson" not in request.args):
+        return jsonify({
+            "message": "Please sent GET or POST query with `intentjson` keys"
+        })
+    intentclf = IntentClassifier(embedder)
+
+    questions, answers = [], []
+
+    # parse data from json
+    try:
+        data = json.loads(request.args["intentjson"])
+    except:
+        return jsonify({
+            "message": "Please send correct json object"
+        })
+
+    for label, category in enumerate(data):
+        answer = category["answers"][0]
+
+        for question in category["questions"]:
+            questions.append(question)
+            answers.append(answer)
+
+    if len(data) < 2:
+        return jsonify({
+            "message": "For training the model requires 2 or more intents"
+        })
+
+    new_model_id = get_models_ids(models_storage)[-1] + 1
+    intentclf.save(models_storage + 'model-{}.pickle'.format(new_model_id))
+    logging.info('saved model with ID {}'.format(new_model_id))
+
+    return jsonify({
+        "message": 'Created model with ID {}'.format(new_model_id)
     })
 
 
