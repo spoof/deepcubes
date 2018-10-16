54a0256d0a63ed26912a633d0e059c7b4f7217a7
sm-svyat <sm.svyat@yandex.ru>
2018-10-16 12:52:26 +0000

    Modified script for models creation

--- scripts/check.py
+++ scripts/check.py
@@ -3,61 +3,75 @@ from intentclf.models import IntentClassifier
 
 import json
 import os
+import argparse
 
 import pandas as pd
+
+MODEL_STORAGE = 'scripts/models/'
 
 embedder = Embedder(os.environ['INTENT_CLASSIFIER_MODEL'])
 classifier = IntentClassifier(embedder)
 
 
-# JSON for live dialog
-questions, answers = [], []
+def main(csv_path=None, json_path=None, trash_questions_path=None):
+    new_model_id = None
+    if json_path:
+        questions, answers = [], []
 
-# parse data from json
-with open("data/dialog.ru.32.json", "r") as handle:
-    data = json.load(handle)
+        # parse data from json
+        with open(json_path, "r") as handle:
+            data = json.load(handle)
 
-for label, category in enumerate(data):
-    # answer = category["name"]
-    answer = category["answers"][0]
+        for label, category in enumerate(data):
+            answer = category["answers"][0]
 
-    for question in category["questions"]:
-        questions.append(question)
-        answers.append(answer)
+            for question in category["questions"]:
+                questions.append(question)
+                answers.append(answer)
 
-model_path = "scripts/models/model-1.pickle"
+        classifier.train(questions, answers)
+        classifier.threshold_calc(trash_questions_path)
+        new_model_id = classifier.save(MODEL_STORAGE)
 
-classifier.train(questions, answers)
-classifier.save(model_path)
+    if csv_path:
+        questions, answers = [], []
 
-# Data for 7 call-center categories
-questions, answers = [], []
+        # parse from pandas data frame
+        data = pd.read_csv(csv_path)
+        for column in data.columns:
+            values = data.loc[
+                ~pd.isnull(data[column])
+            ][column].values
 
-# parse from pandas data frame
-data = pd.read_csv("data/vera_ml_7cat.csv")
-for column in data.columns:
-    values = data.loc[
-        ~pd.isnull(data[column])
-    ][column].values
+            answer = values[0].strip()
+            for question in values[1:]:
+                questions.append(question)
+                answers.append(answer)
 
-    answer = values[0].strip()
-    for question in values[1:]:
-        questions.append(question)
-        answers.append(answer)
+        classifier.train(questions, answers)
+        classifier.threshold_calc(trash_questions_path)
+        new_model_id = classifier.save(MODEL_STORAGE)
 
-model_path = "scripts/models/model-2.pickle"
+    if new_model_id is not None:
+        print('Created model with id {}'.format(new_model_id))
 
-classifier.train(questions, answers)
-classifier.save(model_path)
 
-# classifier.load(model_path)
-
-questions = [
-    "какая у вас зарплата?",
-    "можно ли обучаться в компании?",
-    "работать по выходным можно?"
-]
-
-for question in questions:
-    answer = classifier.predict(question)
-    print("{} : {}".format(question, answer))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Train new model of intent classifier'
+    )
+    parser.add_argument(
+        '-j', '--json_path', required=False, type=str
+    )
+    parser.add_argument(
+        '-c', '--csv_path', required=False, type=str
+    )
+    parser.add_argument(
+        '-t', '--trash_questions_path', required=False, type=str
+    )
+    args = parser.parse_args()
+    main(
+        json_path=args.json_path,
+        csv_path=args.csv_path,
+        trash_questions_path=args.trash_questions_path
+    )
