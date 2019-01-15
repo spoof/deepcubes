1bb3fdb0e36a7bbb766d575038d6825133a3faa9
Dmitry Ischenko <ischenko.dmitry@gmail.com>
2019-01-15 18:26:12 +0300

    Stylish and comments in vera_live servie

--- scripts/vera_live_dialog_service.py
+++ scripts/vera_live_dialog_service.py
@@ -39,11 +39,14 @@ def load_model(model_id):
         model_path = os.path.join(
             MODEL_STORAGE, "{}/vera_live_dialog.cube".format(model_id)
         )
+
         if not os.path.isfile(model_path):
             return False
+
         if model_id not in models:
             model = VeraLiveDialog.load(model_path)
             models[model_id] = model
+
         return True
 
 
@@ -51,6 +54,7 @@ def get_new_model_id(path):
     models_ids = [int(file_name) for file_name in os.listdir(path) if not (
         os.path.isfile(os.path.join(path, file_name))
     )]
+
     sorted_ids = sorted(models_ids)
     new_model_id = sorted_ids[-1] + 1 if len(sorted_ids) else 0
 
@@ -61,9 +65,9 @@ def get_new_model_id(path):
 def predict():
     try:
         if (
-            request.method != "GET" or
-            "model_id" not in request.args or
-            "query" not in request.args
+            request.method != "GET"
+            or "model_id" not in request.args
+            or "query" not in request.args
         ):
             return jsonify({
                 "message": "Please sent GET query with `model_id` and `query` keys",
@@ -75,6 +79,7 @@ def predict():
             model = models[model_id]
         else:
             loading = load_model(model_id)
+
             if loading:
                 model = models[model_id]
             else:
@@ -87,6 +92,7 @@ def predict():
             labels = request.args.get("labels")
         else:
             labels = list()
+
         logging.info('predicting intent for question: {}'.format(query))
 
         model_answer = model(query, labels)
@@ -102,7 +108,9 @@ def predict():
             logging.info('probability: {}'.format(probability))
 
         return jsonify(output)
+
     except Exception as e:
+        # TODO: FIX. dangerous. loging?
         print(e)
 
 
@@ -110,9 +118,8 @@ def predict():
 def train():
     try:
         if (
-            request.method not in ["GET", "POST"] or
-            ("config" not in request.args and
-                "config" not in request.form)
+            request.method not in ["GET", "POST"]
+            or ("config" not in request.args and "config" not in request.form)
         ):
             return jsonify({
                 "message": ("Please sent GET or POST query with `config` key"),
@@ -126,9 +133,7 @@ def train():
         elif 'config' in request.form:
             config = json.loads(request.form["config"])
         else:
-            return jsonify({
-                "message": "Please send correct json object"
-            })
+            return jsonify({"message": "Please send correct json object"})
 
         live_dialog_model.train(config)
 
@@ -144,13 +149,15 @@ def train():
             "message": 'Created model with model_id {}'.format(new_model_id),
             "model_id": new_model_id,
         })
+
     except Exception as e:
+        # TODO: FIX. dangerous. loging?
         print(e)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='Launch vera live dialog API'
+        description='Launch Vera Live Dialog API'
     )
 
     parser.add_argument(
@@ -160,6 +167,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
     for model_id in args.model_id_list:
         loading = load_model(model_id)
+
         if not loading:
             print("Model with id {} not found".format(model_id))
+
     app.run(host="0.0.0.0", port=3339, debug=False)
