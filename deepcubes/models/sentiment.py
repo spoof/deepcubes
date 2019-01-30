59a14faba3ae0ab10ec8aef7a4ffb16c748cdc1a
Dmitry Ischenko <ischenko.dmitry@gmail.com>
2019-01-30 12:21:06 +0000

    Add torch sentiment model

--- deepcubes/models/sentiment.py
+++ deepcubes/models/sentiment.py
@@ -1,8 +1,34 @@
 import json
 import os
 
+from torch import nn
+
 from ..cubes import TrainableCube, PredictorCube
 from ..cubes import EditDistanceMatcher
+
+
+class SentimentNN(nn.Module):
+
+    def __init__(self, num_tokens, embed_size, hidden_size,
+                 num_layers, num_classes):
+        super().__init__()
+
+        self.embed_size = embed_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.embed = nn.Embedding(num_tokens, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers,
+                            batch_first=True, bidirectional=True)
+        self.fc = nn.Linear(hidden_size * 2, num_classes)
+
+    def forward(self, x, hidden=None):
+        emb = self.embed(x)
+
+        out, (ht, ct) = self.lstm(emb, hidden)
+        out = self.fc(out[:, -1, :])
+
+        return out
 
 
 class Sentiment(TrainableCube, PredictorCube):
--- setup.py
+++ setup.py
@@ -26,5 +26,5 @@ setup(
 
     keywords='nlp',
     install_requires=["gensim", "sklearn", "pandas", "pymystem3",
-                      "numpy", "flask", "editdistance"],
+                      "numpy", "flask", "editdistance", "torch"],
 )
