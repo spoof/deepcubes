c61dc26aebf9ac40b3b5534c5deed1c2e339128a
Dmitry Ischenko <ischenko.dmitry@gmail.com>
2019-02-08 11:45:21 +0000

    Added embedder service tests

---
+++ tests/test_embedder_service.py
@@ -0,0 +1,31 @@
+import unittest
+import numpy as np
+
+import scripts.embedder_service as service
+from deepcubes.cubes import Tokenizer
+
+
+class VeraLiveDialogServiceTest(unittest.TestCase):
+
+    def setUp(self):
+        service.app.testing = True
+        self.service = service.app.test_client()
+
+    def test_get_requests(self):
+        response = self.service.get('/test')
+        self.assertEquals(response.status_code, 200)
+
+        tokenizer = Tokenizer()
+        tokenizer.train('lem')
+
+        tokens = tokenizer('Робот Вера')
+        response = self.service.get('/test', query_string={"tokens": tokens})
+        print(response.get_json())
+
+        generated_vector = response.get_json()["vector"]
+
+        np.testing.assert_almost_equal(
+            sum(generated_vector),
+            -0.030646920857179794,
+            1
+        )
